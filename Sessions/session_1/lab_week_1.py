ce313a229d51a32b396b6b5b7a83bacedeaf6a29
ydwisdom <b01834170@studentmail.uws.ac.uk>
2026-03-20 11:46:42 +0000

    setup
    starting folders

---
+++ Sessions/session_1/lab_week_1.py
@@ -0,0 +1,12 @@
+# Your first Python program
+print("Hello. Welcome to the ISD Module.")
+print("This program has been run successfully.")
+
+
+class_name = "ISD"
+number_of_students = 20
+my_string = "Welcome to the " + class_name + " module. There are " + str(number_of_students) + " students in this class."
+print(my_string)
+my_string = f"Welcome to the {class_name} module. There are {number_of_students} students in this class."
+print(my_string)
+# The above two lines are equivalent. The second one is more readable and is the preferred way to format strings in Python 3.6 and above.
